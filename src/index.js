3e91b6e54431dd63a9d6da94687247f75c3edbd3
Henrick Mello <hmartins505@gmail.com>
2017-05-04 13:19:04 -0300

    Forgot to DELET THIS

--- src/index.js
+++ src/index.js
@@ -4,14 +4,4 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/index.css";
 
-class About extends Component {
-  render() {
-    return (
-      <div>
-        aaaaaaaaaaa
-      </div>
-    );
-  }
-}
-
 render(<App />, document.getElementById("root"));
